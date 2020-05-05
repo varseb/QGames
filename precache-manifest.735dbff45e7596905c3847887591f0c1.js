a778a8c0a020ea6beed23183d6039791f8ffb76c
seb <varseb@gmail.com>
2020-05-05 18:50:21 -0300

    first version

---
+++ precache-manifest.735dbff45e7596905c3847887591f0c1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b2b75fe480f4ce7cb8d4b089e06ef52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baa9f69ace16dbddfbdd",
+    "url": "/static/css/main.5cd93b73.chunk.css"
+  },
+  {
+    "revision": "ea317b2d549ff3f1cc64",
+    "url": "/static/js/2.51c6a7ad.chunk.js"
+  },
+  {
+    "revision": "baa9f69ace16dbddfbdd",
+    "url": "/static/js/main.87af66f4.chunk.js"
+  },
+  {
+    "revision": "ba199fd4251af8ce65bd",
+    "url": "/static/js/runtime-main.67f6076d.js"
+  },
+  {
+    "revision": "e1996192b98a516646ff9a8c0c0ca90c",
+    "url": "/static/media/Oswald-Regular.e1996192.ttf"
+  },
+  {
+    "revision": "60b83e8d22ae1d94d0204d3b95ad2961",
+    "url": "/static/media/countdown.60b83e8d.mp3"
+  },
+  {
+    "revision": "5f50be94e09bfb913a5e36701db6a01c",
+    "url": "/static/media/fail.5f50be94.mp3"
+  },
+  {
+    "revision": "1e009f81beb1844f252c4642729b383a",
+    "url": "/static/media/timeout.1e009f81.mp3"
+  },
+  {
+    "revision": "2bf45ec977e9874fa96fec59cf44dbda",
+    "url": "/static/media/won3.2bf45ec9.mp3"
+  }
+]);
